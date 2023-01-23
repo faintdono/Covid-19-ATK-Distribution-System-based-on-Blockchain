707972cc83a45fd6b8c4e22b1ef7e61d0756db95
raned.chu <60337923+faintdono@users.noreply.github.com>
2023-01-23 22:41:14 +0700

    delete invoking test account priv

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,15 +4,15 @@ require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 const {besu_dev_acc_priv1, besu_dev_acc_priv2} = process.env;
 
-// const { task } = require("hardhat/config");
+const { task } = require("hardhat/config");
 
-// task("accounts", "Prints the list of accounts", async () => {
-//   const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
 
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 module.exports = {
   solidity: "0.8.17",
