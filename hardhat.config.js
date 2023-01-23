26437f90f2cd27e11c562bcf4c34431f432a7cf8
6210612823 <6210612823@student.tu.ac.th>
2023-01-23 22:25:49 +0700

    hardhat config local
    Use npx hardhat node to run local accounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-truffle5");
-require("@nomiclabs/hardhat-ethers");
-require("dotenv").config();
-const {besu_dev_acc_priv1, besu_dev_acc_priv2} = process.env;
 
-// const { task } = require("hardhat/config");
-
-// task("accounts", "Prints the list of accounts", async () => {
-//   const accounts = await ethers.getSigners();
-
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
-
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks: {
-    besu_dev: {
-      url: "http://localhost:18545",
-      accounts: [
-        `${besu_dev_acc_priv1}`,
-        `${besu_dev_acc_priv2}`,
-      ],
-    },
-  },
 };
