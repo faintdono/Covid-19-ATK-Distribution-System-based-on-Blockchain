3d3abd83d87536b549f5bd81a69722e347181d55
6210612823 <6210612823@student.tu.ac.th>
2023-01-24 23:14:17 +0700

    Modified to use on-local

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,4 @@
 require("@nomicfoundation/hardhat-toolbox");
-<<<<<<< HEAD
-=======
-require("@nomiclabs/hardhat-truffle5");
-require("@nomiclabs/hardhat-ethers");
-require("dotenv").config();
-const {besu_dev_acc_priv1, besu_dev_acc_priv2} = process.env;
-
-const { task } = require("hardhat/config");
-
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
->>>>>>> main
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,23 +3,6 @@ const { ethers } = require("hardhat");
 require("dotenv").config();
 
 async function main() {
-<<<<<<< HEAD
-
-  const signer = await ethers.getSigner()
-  const deployer = signer;
-  console.log("Deploying contracts with the account:", deployer.address);
-
-  const Product = await ethers.getContractFactory("Manufacturer");
-  const product = await Product.deploy();
-  console.log("Contract address:", product.address);
-=======
-  // const { besu_dev_acc_priv1, besu_dev_acc_priv2 } = process.env;
-  // const provider = new ethers.providers.JsonRpcProvider(
-  //   "http://localhost:18545"
-  // );
-  // const signer = new ethers.Wallet(besu_dev_acc_priv1, provider);
-  // const deployer = signer;
-
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
 
@@ -29,7 +12,6 @@ async function main() {
   const Man = await ethers.getContractFactory("Manufacturer");
   const man = await Man.deploy(registry.address);
   console.log("Manufacturer address:", man.address);
->>>>>>> main
 }
 
 main()
