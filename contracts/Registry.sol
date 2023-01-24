bc0aaaff1cf1125935e8961d21746df1534aa1c9
raned.chu <raned.chu@gmail.com>
2023-01-24 20:28:45 +0700

    update

---
+++ contracts/Registry.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.8.0;
+
+contract Registry {
+    address public owner;
+    uint256 public count = 0;
+
+    struct stakeholder {
+        uint256 id;
+        address stakeAddress;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    mapping(uint256 => stakeholder) private stakeholders;
+
+    uint256[] stakeArray;
+
+    function createStakeholder(address _stakeholder) public {
+        count++;
+        stakeholders[count] = stakeholder(count, _stakeholder);
+        stakeArray.push(count);
+    }
+
+    function getID() public view returns (uint256[] memory) {
+        return stakeArray;
+    }
+
+    function getAddress(uint256 _id) private view returns (address) {
+        return stakeholders[_id].stakeAddress;
+    }
+
+    function checkStakeholders(address _input) public view returns (bool) {
+        uint256 i = 0;
+        while (i < stakeArray.length) {
+            if (getAddress(i) == _input) {
+                return true;
+            }
+            i++;
+        }return false;
+    }
+}
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,24 @@
+const { ethers } = require("hardhat");
+
 require("dotenv").config();
 
 async function main() {
-  const { besu_dev_acc_priv1, besu_dev_acc_priv2 } = process.env;
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://localhost:18545"
-  );
-  const signer = new ethers.Wallet(besu_dev_acc_priv1, provider);
-  const deployer = signer;
+  // const { besu_dev_acc_priv1, besu_dev_acc_priv2 } = process.env;
+  // const provider = new ethers.providers.JsonRpcProvider(
+  //   "http://localhost:18545"
+  // );
+  // const signer = new ethers.Wallet(besu_dev_acc_priv1, provider);
+  // const deployer = signer;
 
+  const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
-  const Product = await ethers.getContractFactory("Manufacturer");
-  const product = await Product.deploy();
-  console.log("Contract address:", product.address);
-  
+
+  const Registry = await ethers.getContractFactory("Registry");
+  const registry = await Registry.deploy();
+  console.log("Registry address:", registry.address);
+  const Man = await ethers.getContractFactory("Manufacturer");
+  const man = await Man.deploy(registry.address);
+  console.log("Manufacturer address:", man.address);
 }
 main()
   .then(() => process.exit(0))
