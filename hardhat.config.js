d500c6975f391c2065202dd70dc961f014d974df
raned.chu <raned.chu@gmail.com>
2023-01-31 16:42:42 +0700

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
-const {besu_dev_acc_priv1, besu_dev_acc_priv2} = process.env;
+const {
+  besu_dev_acc_priv1,
+  besu_dev_acc_priv2,
+  GOERLI_ACC_PRIV1,
+  GOERLI_ACC_PRIV2,
+  GOERLI_ACC_PRIV3,
+  GOERLI_ACC_PRIV4,
+} = process.env;
 
 const { task } = require("hardhat/config");
 
@@ -19,9 +26,15 @@ module.exports = {
   networks: {
     besu_dev: {
       url: "http://localhost:18545",
+      accounts: [`${besu_dev_acc_priv1}`, `${besu_dev_acc_priv2}`],
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/9352c4ea35594880b179f70ece40051e",
       accounts: [
-        `${besu_dev_acc_priv1}`,
-        `${besu_dev_acc_priv2}`,
+        `${GOERLI_ACC_PRIV1}`,
+        `${GOERLI_ACC_PRIV2}`,
+        `${GOERLI_ACC_PRIV3}`,
+        `${GOERLI_ACC_PRIV4}`,
       ],
     },
   },
