72646cb479cbbeae083b3984e5ce4acba4e75700
6210612823 <60337806+6210612823@users.noreply.github.com>
2023-02-05 21:53:47 +0700

    refactor smart contract 2

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,42 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const {
+  besu_dev_acc_priv1,
+  besu_dev_acc_priv2,
+  GOERLI_ACC_PRIV1,
+  GOERLI_ACC_PRIV2,
+  GOERLI_ACC_PRIV3,
+  GOERLI_ACC_PRIV4,
+} = process.env;
+
+const { task } = require("hardhat/config");
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    besu_dev: {
+      url: "http://localhost:18545",
+      accounts: [`${besu_dev_acc_priv1}`, `${besu_dev_acc_priv2}`],
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/9352c4ea35594880b179f70ece40051e",
+      accounts: [
+        `${GOERLI_ACC_PRIV1}`,
+        `${GOERLI_ACC_PRIV2}`,
+        `${GOERLI_ACC_PRIV3}`,
+        `${GOERLI_ACC_PRIV4}`,
+      ],
+    },
+  },
 };
