ce8f23c300606b896028ef9517a8aa8f39451773
6210612823 <6210612823@student.tu.ac.th>
2023-03-21 02:03:29 +0700

    push something that doesn't work

--- backend/test/supplychain.js
+++ backend/test/supplychain.js
@@ -23,8 +23,63 @@ describe("Supply Chain", () => {
     return result;
   }
 
-  async function generateRandomAmount() {
+  async function getUserKey(address) {
+    const result = supplychain.getUserKey(address);
+    return result;
+  }
+
+  async function getRootKey(address) {
+    const result = supplychain.getRootKey(address);
+    return result;
+  }
+
+  async function getLedger(address) {
+    const result = supplychain.getLedger(address);
+    return result;
+  }
+
+  function generateRandomAmount() {
     return Math.floor(Math.random() * 100) + 1;
+  }
+
+  function mockUpLedgerKey(
+    owner,
+    sellerAddress,
+    orderID,
+    invoice,
+    lotID,
+    sku,
+    key,
+    amount
+  ) {
+    const encoded = web3.eth.abi.encodeParameters(
+      [
+        "address",
+        "address",
+        "string",
+        "string",
+        "string",
+        "string",
+        "bytes32",
+        "uint256",
+      ],
+      [owner, sellerAddress, orderID, invoice, lotID, sku, key, amount]
+    );
+    const hash = web3.utils.sha3(encoded, { encoding: "hex" });
+
+    return hash;
+  }
+
+  function convertBigNumber(obj) {
+    const obj2 = [];
+    for (const key in obj) {
+      if (typeof obj[key] === "object") {
+        obj2[key] = obj[key].toNumber();
+      } else {
+        obj2[key] = obj[key];
+      }
+    }
+    return obj2;
   }
 
   async function generateRandomOrderID(userAddress) {
@@ -150,5 +205,77 @@ describe("Supply Chain", () => {
         );
       expect(result).to.emit(supplychain, "NewProduct");
     });
+  });
+
+  describe("Sell Product", () => {
+    it("Get LedgerKey", async () => {
+      supplychain
+        .connect(manufacturer)
+        .addProduct(
+          lotID,
+          sku,
+          manufacturerName,
+          manufacturingDate,
+          expiryDate,
+          productAmount
+        );
+      const keys = await getUserKey(manufacturer.address);
+    });
+
+    it("Sell Product", async () => {
+      supplychain
+        .connect(manufacturer)
+        .addProduct(
+          lotID,
+          sku,
+          manufacturerName,
+          manufacturingDate,
+          expiryDate,
+          productAmount
+        );
+      const keys = await getUserKey(manufacturer.address);
+      const ledger101 = await getLedger(keys[0]);
+      const ledger = convertBigNumber(ledger101);
+      console.log(ledger[0], ledger[1]);
+      //console.log("manufacturer (ROOT) key = ", keys[0]);
+      const test = mockUpLedgerKey(
+        ledger[0],
+        ledger[1],
+        ledger[2],
+        ledger[3],
+        ledger[4],
+        ledger[5],
+        ledger[6]
+      );
+      console.log(test);
+    });
+
+    /*
+        it('Sell Product', async () => {
+            supplychain.connect(manufacturer).addProduct(lotID, sku, manufacturerName, manufacturingDate, expiryDate, productAmount)
+            const keys = await getUserKey(manufacturer.address)
+            const ledger1 = await getLedger(keys[0])
+            console.log(ledger1)
+            console.log('manufacturer (ROOT) key = ', keys[0])
+            console.log('===========================')
+
+            const orderID = generateRandomOrderID(distributor.address)
+            await ordermanagement.connect(distributor).createOrder(orderID, manufacturer.address, productAmount)
+            // const date = await getBlockTimestamp()
+
+            const invoice = generateRandomString()
+
+            await ordermanagement.connect(manufacturer).confirmOrder(orderID, invoice, lotID, sku)
+            await supplychain.connect(manufacturer).sellProduct(orderID, keys[0])
+
+            const keys2 = await getUserKey(distributor.address)
+            const ledger2 = await getLedger(keys2[0])
+            const rootKey = await getRootKey(keys2[0])
+            console.log(ledger2)
+            console.log('distributor key = ', keys2[0])
+            console.log('get ROOT key = ', rootKey)
+
+        })
+        */
   });
 });
