d04dabd8c2e55084969968a86a4b569e61c18569
raned.chu <raned.chu@gmail.com>
2023-03-31 22:54:09 +0700

    add smart contract abi

--- frontend/my-eth-app/packages/contracts/src/addresses.js
+++ frontend/my-eth-app/packages/contracts/src/addresses.js
@@ -1,7 +1,8 @@
-// This address points to a dummy ERC-20 contract. Replace it with your own smart contracts.
 const addresses = {
-  registraton: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-  orderManagement: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
-  supplyChain: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+  registraton: "0x9bb943e37e9C3D1f0e0d3E390db4E9392dd74B3e",
+  orderManagement: "0x8D5e2B7a46a279026f2608d741ECBF9Cb681b709",
+  supplyChain: "0x5569d73BF3F23F411cAb2F9AbB059c94A7033D38",
 };
+
 export default addresses;
+0x984b88e1c2b08f073964ecdb6c9a5773207e352d;
