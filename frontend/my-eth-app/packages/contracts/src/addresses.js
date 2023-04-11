a22e21353601bdd8204a04b9a85e65af2fce1206
raned.chu <raned.chu@gmail.com>
2023-04-11 21:06:36 +0700

    update abi file

--- frontend/my-eth-app/packages/contracts/src/addresses.js
+++ frontend/my-eth-app/packages/contracts/src/addresses.js
@@ -1,7 +1,11 @@
+// const addresses = {
+//   registraton: "0x9bb943e37e9C3D1f0e0d3E390db4E9392dd74B3e",
+//   orderManagement: "0x3C8F8c94cC509495276c6b9783Dafb36B14CD003",
+//   supplyChain: "0x5569d73BF3F23F411cAb2F9AbB059c94A7033D38",
+// };
 const addresses = {
-  registraton: "0x9bb943e37e9C3D1f0e0d3E390db4E9392dd74B3e",
-  orderManagement: "0x3C8F8c94cC509495276c6b9783Dafb36B14CD003",
-  supplyChain: "0x5569d73BF3F23F411cAb2F9AbB059c94A7033D38",
+  registraton: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+  orderManagement: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+  supplyChain: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
 };
-
 export default addresses;
