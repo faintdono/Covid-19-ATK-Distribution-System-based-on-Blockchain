baf381e8873f59c3541281e0bf15b545f67efc4e
raned.chu <60337923+faintdono@users.noreply.github.com>
2023-04-20 00:24:48 +0700

    update

--- frontend/my-eth-app/packages/react-app/src/features/SupplyChain/components/SellProduct/ModalFormWrapped/index.js
+++ frontend/my-eth-app/packages/react-app/src/features/SupplyChain/components/SellProduct/ModalFormWrapped/index.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import useSellProduct from "../../../hooks/useSellProduct";
+import useGetter from "../../../hooks/useGetter";
+import { useEthers } from "@usedapp/core";
 
 const Modal = ({ setOpenModal }) => {
-  const { send: sellProduct } = useSellProduct();
+  const { account } = useEthers();
 
+  const [LedgerKey, setLedgerKey] = useState("");
+
+  const { send: sellProduct } = useSellProduct();
+  const values = useGetter("getUserKey", account);
+  const info = useGetter("getProduct", LedgerKey);
+
+  let product = [];
+  let Keys = [];
+  if (values !== undefined) {
+    Keys = values;
+  }
+  if (info === undefined) {
+    product = ["None", "None", "None", "None", "None", "None", "None"];
+  } else {
+    product = info;
+  }
   return (
     <div className="modal is-active">
       <div
@@ -24,16 +42,68 @@ const Modal = ({ setOpenModal }) => {
           ></button>
         </header>
         <section class="modal-card-body">
-          <div className="field">
-            <label className="label">Order ID</label>
-            <div className="control has-text-black">
-              <input className="input" type="text" id="OrderID" />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Key</label>
-            <div className="control">
-              <input className="input" type="text" id="Key" />
+          <div className="columns">
+            <div className="column">
+              <div className="field">
+                <label className="label">Order ID</label>
+                <div className="control has-text-black">
+                  <input className="input" type="text" id="OrderID" />
+                </div>
+              </div>
+              <div class="field">
+                <label className="label">Key</label>
+                <div class="control">
+                  <div className="select">
+                    <select
+                      id="Key"
+                      onChange={() => {
+                        setLedgerKey(document.getElementById("Key").value);
+                      }}
+                    >
+                      <option value="0x">Select a key</option>
+                      {Keys.map((key) => (
+                        <option value={key}>{key}</option>
+                      )).reverse()}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="columns">
+                <div className="column">
+                  <br />
+                  <div className="subtitle is-5 has-text-centered">
+                    Product Associate with Selected Key
+                  </div>
+                  <div className="field">
+                    <label className="label">Lot ID</label>
+                    <div className="control">{product[0].toString()}</div>
+                  </div>
+                  <div className="field">
+                    <label className="label">SKU</label>
+                    <div className="control">{product[1].toString()}</div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Manufacturer Name</label>
+                    <div className="control">{product[2].toString()}</div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Manufacturer Address</label>
+                    <div className="control">{product[3].toString()}</div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Manufacturering Date</label>
+                    <div className="control">{product[4].toString()}</div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Expiration Date</label>
+                    <div className="control">{product[5].toString()}</div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Totol Amount</label>
+                    <div className="control">{product[6].toString()}</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
